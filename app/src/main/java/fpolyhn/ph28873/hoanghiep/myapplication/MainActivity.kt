faa8908bb073127548b3b512b4467bb5d6f68ee2
root <nhh.tcp@gmail.com>
2023-03-12 10:47:51 +0700

    project h2clt

---
+++ app/src/main/java/fpolyhn/ph28873/hoanghiep/myapplication/MainActivity.kt
@@ -0,0 +1,11 @@
+package fpolyhn.ph28873.hoanghiep.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
